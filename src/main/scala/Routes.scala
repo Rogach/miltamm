f497290f1a6ab82d68ce6d7b1e74cd85d7031f79
Rogach <platon7pronko@gmail.com>
2013-01-15 18:12:12 +0400

    Moved Actions in separate source file

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -1,7 +1,6 @@
 package org.rogach.miltamm
 
 import java.io.File
-import org.apache.commons.io.FileUtils
 
 trait Routes {
   
@@ -25,22 +24,3 @@ case class Path(p: Seq[String]) {
   override def toString = p.mkString("/")
 }
 
-trait Action {
-  def apply(prefix: Path, fullName: String)
-}
-object Actions {
-  object Ignore extends Action {
-    def apply(prefix: Path, fullName: String) = {}
-  }
-  object Copy extends Action {
-    def apply(prefix: Path, fullName: String) = {
-      FileUtils.copyFile(new File(Main.opts.template() + "/" + fullName), new File(Main.opts.destination() + "/" + fullName))
-    }
-  }
-  class Move(from: Path, to: Path) extends Action {
-    def apply(prefix: Path, fullName: String) = {
-      val dest = Path(prefix.p.dropRight(from.p.size) ++ to.p)
-      FileUtils.copyFile(new File(Main.opts.template() + "/" + fullName), new File(Main.opts.destination() + "/" + dest))
-    }
-  }
-}
